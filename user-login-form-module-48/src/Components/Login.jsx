88607a39b19e56984f0038eaeb7aa58e32a232b2
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-10-16 20:30:47 +0600

    Login form & added Forgot Password && more bug fixed

--- user-login-form-module-48/src/Components/Login.jsx
+++ user-login-form-module-48/src/Components/Login.jsx
@@ -1,11 +1,102 @@
-import React from 'react';
+import {
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import React, { useRef, useState } from "react";
+import { Link } from "react-router";
+import { auth } from "../firebase/firebase";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
+  const [error, setError] = useState("");
+  //   const [success, setSuccess] = useState(false);
+  const emailRef = useRef();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    console.log(email, password);
+
+    setError("");
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        if (!result.user.emailVerified) {
+          alert("Please verify your email address");
+        }
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+  const handleForgetPassword = () => {
+    const email = emailRef.current.value;
+    console.log(email);
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        alert("Please check your email");
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
         </div>
-    );
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <form onSubmit={handleLogin}>
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  className="input"
+                  ref={emailRef}
+                  placeholder="Email"
+                />
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  className="input"
+                  placeholder="Password"
+                />
+                <div onClick={handleForgetPassword}>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button className="btn btn-neutral mt-4">Login</button>
+              </fieldset>
+              <div>
+                {error && <p className="text-red-500">{error} </p>}
+                {/* {success && (
+                  <p className="text-green-400 font-bold">
+                    Account Successfully
+                  </p>
+                )} */}
+              </div>
+            </form>
+            <p>
+              New to our Website? Please{" "}
+              <Link
+                to="/register"
+                className="text-blue-500 font-semibold underline"
+              >
+                Register
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- user-login-form-module-48/src/Components/Register.jsx
+++ user-login-form-module-48/src/Components/Register.jsx
@@ -1,30 +1,70 @@
 import React, { useState } from "react";
 import { auth } from "../firebase/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from "firebase/auth";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router";
 
 const Register = () => {
   const [error, setError] = useState("");
   const [success, setSuccess] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleRegister = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.pass.value;
-    console.log(email, password);
+    const terms = e.target.terms.checked;
+    console.log(email, password, terms);
+
+    // Password "regEx" ..............
+
+    const passwordPattern = /^.{6,}$/;
+    const passwordAllPattern =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9]).{7,}$/;
+
+    if (!passwordPattern.test(password)) {
+      console.log("password don't match");
+      setError("Password must be 6 character");
+      return;
+    }
+
+    if (!passwordAllPattern.test(password)) {
+      setError(
+        "Password must be over 6 characters, contain upper & lower case letters, and at least one special character!"
+      );
+      return;
+    }
 
     // reset error or success
     setError("");
     setSuccess(false);
 
+    if (!terms) {
+      setError("Please accept our terms and condition");
+      return;
+    }
+
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
-        console.log(result);
+        console.log(result.user);
         setSuccess(true);
-        e.target.reset()
+        e.target.reset();
+
+        sendEmailVerification(result.user).then(() => {
+          alert("Please login your email and verify your address");
+        });
       })
       .catch((error) => {
         setError(error.message);
       });
+  };
+
+  const handleTogglePassword = (e) => {
+    e.preventDefault();
+    setShowPassword(!showPassword);
   };
 
   return (
@@ -37,6 +77,15 @@ const Register = () => {
           <div className="card-body">
             <form onSubmit={handleRegister}>
               <fieldset className="fieldset">
+                {/* Name */}
+                <label className="label">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  className="input"
+                  placeholder="Your Name"
+                />
+                {/* Email  */}
                 <label className="label">Email</label>
                 <input
                   type="email"
@@ -45,25 +94,49 @@ const Register = () => {
                   placeholder="Email"
                 />
                 <label className="label">Password</label>
-                <input
-                  type="password"
-                  name="pass"
-                  className="input"
-                  placeholder="Password"
-                />
-                <div>
-                  <a className="link link-hover">Forgot password?</a>
+                <div className="relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    name="pass"
+                    className="input"
+                    placeholder="Password"
+                  />
+                  <button
+                    onClick={handleTogglePassword}
+                    className="absolute right-6 top-3"
+                  >
+                    {showPassword ? (
+                      <FaEye size={15} />
+                    ) : (
+                      <FaEyeSlash size={15} />
+                    )}
+                  </button>
                 </div>
-                <button className="btn btn-neutral mt-4">Login</button>
-              </fieldset>
-            </form>
+                {/* Check Box */}
+                <div>
+                  <label className="label">
+                    <input type="checkbox" name="terms" className="checkbox" />
+                    Remember me
+                  </label>
+                </div>
 
-            <div>
-              {error && <p className="text-red-500">{error} </p>}
-              {success && (
-                <p className="text-green-400 font-bold">Account Successfully</p>
-              )}
-            </div>
+                <button className="btn btn-neutral mt-4">Register</button>
+              </fieldset>
+              <div>
+                {error && <p className="text-red-500">{error} </p>}
+                {success && (
+                  <p className="text-green-400 font-bold">
+                    Account Successfully
+                  </p>
+                )}
+              </div>
+            </form>
+            <p>
+              Already have an account? Please{" "}
+              <Link to="/login" className="text-blue-500 font-semibold">
+                Login
+              </Link>
+            </p>
           </div>
         </div>
       </div>
